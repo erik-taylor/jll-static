6f1e6118e3913e3c5976c46aa681a17e5cd35cbc
Erik Taylor <etaylor@usptm013.na.acushnetgolf.com>
2019-03-06 15:38:27 -0800

    HTML restructure, blog styling changes

--- assets/hbs_templates/body.js
+++ assets/hbs_templates/body.js
@@ -1,13 +1,9 @@
-define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
-    return "dark";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+define(["handlebars"], function(Handlebars) { return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = container.invokePartial(partials.header,depth0,{"name":"header","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "<div class=\"container-fluid "
-    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.dark_theme : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\">\n"
+  return "<!DOCTYPE html>\n<html>\n<head>\n"
+    + ((stack1 = container.invokePartial(partials.head,depth0,{"name":"head","data":data,"indent":"\t","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "</head>\n\n"
     + ((stack1 = container.invokePartial(partials["@partial-block"],depth0,{"name":"@partial-block","data":data,"indent":"    ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
-    + "</div>\n"
-    + ((stack1 = container.invokePartial(partials.footer,depth0,{"name":"footer","data":data,"helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "");
+    + "\n</html>\n";
 },"usePartial":true,"useData":true}); });
