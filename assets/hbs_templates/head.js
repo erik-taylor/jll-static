9f15fcc7271e8f0f9d842922bf64964804e758a5
Erik Taylor <etaylor@usptm013.na.acushnetgolf.com>
2019-03-28 10:00:25 -0700

    bLazy full integration, HTML changes for bLazy

--- assets/hbs_templates/head.js
+++ assets/hbs_templates/head.js
@@ -23,20 +23,6 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":fu
 },"13":function(container,depth0,helpers,partials,data) {
     return "http://www.jetlagleah.com/";
 },"15":function(container,depth0,helpers,partials,data) {
-    return "background-image: url("
-    + container.escapeExpression(container.lambda((depth0 != null ? depth0.cover : depth0), depth0))
-    + ");\n";
-},"17":function(container,depth0,helpers,partials,data) {
-    var stack1;
-
-  return "background-image: url("
-    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.background_image : stack1), depth0))
-    + ");\n";
-},"19":function(container,depth0,helpers,partials,data) {
-    return "background-repeat: repeat;\nbackground-position: center center;\n";
-},"21":function(container,depth0,helpers,partials,data) {
-    return "background-size: 100% auto;\nbackground-size: cover;\n";
-},"23":function(container,depth0,helpers,partials,data) {
     var stack1;
 
   return "color: "
@@ -71,15 +57,12 @@ define(["handlebars"], function(Handlebars) { return Handlebars.template({"1":fu
     + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.meta_data : depth0)) != null ? stack1.meta_description : stack1),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.program(5, data, 0),"data":data})) != null ? stack1 : "")
     + "\">\n<meta name=\"twitter:image\" content=\""
     + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.meta_data : depth0)) != null ? stack1.meta_image : stack1),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.program(9, data, 0),"data":data})) != null ? stack1 : "")
-    + "\">\n<meta name=\"twitter:site\" content=\"@JetlagLeah\">\n<meta name=\"twitter:creator\" content=\"@JetlagLeah\">\n\n<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/apple-touch-icon.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon-32x32.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon-16x16.png\">\n<link rel=\"manifest\" href=\"/site.webmanifest\">\n<link rel=\"mask-icon\" href=\"/safari-pinned-tab.svg\" color=\"#5bbad5\">\n<meta name=\"msapplication-TileColor\" content=\"#603cba\">\n<meta name=\"theme-color\" content=\"#ffffff\">\n\n<link rel=\"stylesheet\" href=\"/assets/css/main.css\">\n<link rel=\"stylesheet\" href=\"/assets/css/bootstrap-grid.min.css\">\n\n<script src=\"/assets/js/lax.min.js\"></script>\n<script src=\"/assets/js/main.js\"></script>\n\n<script defer src=\"https://use.fontawesome.com/releases/v5.7.1/js/all.js\" integrity=\"sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7\" crossorigin=\"anonymous\"></script>\n<script src=\"/assets/js/instafeed.min.js\"></script>\n\n<!-- Global site tag (gtag.js) - Google Analytics -->\n<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-135543302-1\"></script>\n<script>\n  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag('js', new Date());\n\n  gtag('config', 'UA-135543302-1');\n</script>\n\n<style>\nbody {\n"
-    + ((stack1 = helpers["if"].call(alias3,(depth0 != null ? depth0.cover : depth0),{"name":"if","hash":{},"fn":container.program(15, data, 0),"inverse":container.program(17, data, 0),"data":data})) != null ? stack1 : "")
-    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.tile_background : stack1),{"name":"if","hash":{},"fn":container.program(19, data, 0),"inverse":container.program(21, data, 0),"data":data})) != null ? stack1 : "")
-    + "font-family: "
+    + "\">\n<meta name=\"twitter:site\" content=\"@JetlagLeah\">\n<meta name=\"twitter:creator\" content=\"@JetlagLeah\">\n\n<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/apple-touch-icon.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/favicon-32x32.png\">\n<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/favicon-16x16.png\">\n<link rel=\"manifest\" href=\"/site.webmanifest\">\n<link rel=\"mask-icon\" href=\"/safari-pinned-tab.svg\" color=\"#5bbad5\">\n<meta name=\"msapplication-TileColor\" content=\"#603cba\">\n<meta name=\"theme-color\" content=\"#ffffff\">\n\n<link rel=\"stylesheet\" href=\"/assets/css/main.css\">\n<link rel=\"stylesheet\" href=\"/assets/css/bootstrap-grid.min.css\">\n\n<script src=\"/assets/js/lax.min.js\"></script>\n<script src=\"/assets/js/blazy.js\"></script>\n<script src=\"/assets/js/main.js\"></script>\n\n<script defer src=\"https://use.fontawesome.com/releases/v5.7.1/js/all.js\" integrity=\"sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7\" crossorigin=\"anonymous\"></script>\n<script src=\"/assets/js/instafeed.min.js\"></script>\n\n<!-- Global site tag (gtag.js) - Google Analytics -->\n<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-135543302-1\"></script>\n<script>\n  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag('js', new Date());\n\n  gtag('config', 'UA-135543302-1');\n</script>\n\n<style>\nbody {\nfont-family: "
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.body_text_font_family : stack1), depth0))
     + ";\n}\nh1, h2, h3, h4, h5, h6, span.siteName {\nfont-family: "
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.headers_font_family : stack1), depth0))
     + ";\n"
-    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.accented_headers : stack1),{"name":"if","hash":{},"fn":container.program(23, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias3,((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.accented_headers : stack1),{"name":"if","hash":{},"fn":container.program(15, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "}\na {\ncolor: "
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.global : depth0)) != null ? stack1.accent_color : stack1), depth0))
     + ";\n}\n.aBackgroundTint {\nbackground: "
